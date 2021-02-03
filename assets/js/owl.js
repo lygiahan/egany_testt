6bea149ade7630c0953fd9671df982bcd693cd63
HanLy <hanjimj000@gmail.com>
2021-02-03 18:50:42 +0700

    first commit

---
+++ assets/js/owl.js
@@ -0,0 +1,88 @@
+/**Owl Carousel evaluate */
+$('.evaluate_theme').owlCarousel({
+    loop:true,
+    margin:30,
+    
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+/**Hot price owl------------------------ */
+$('.hotPrice_theme').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+$('.selling_theme').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+$('.Highlights_theme').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+/** Owl Carousel Intagram */
+$('.intagram_theme').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
